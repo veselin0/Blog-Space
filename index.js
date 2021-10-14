32fe4ea049da3af248facaa2e01b5aa0924c61e7
Veselin <vesselin60@gmail.com>
2021-10-14 09:29:04 +0300

    Display blogs on page

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(data => {
         const postsArr = data.slice(0, 5);
         console.log(postsArr);
+        for (let post of postsArr) {
+            document.querySelector('.post').innerHTML += 
+            `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p> 
+                <hr />   
+            `
+        }
     });
+    
+/**
+ Challenge:
+
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
+ 
+ Hints: 
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you 
+   can then put into the div with `innerHTML`
+ */
+
+   
